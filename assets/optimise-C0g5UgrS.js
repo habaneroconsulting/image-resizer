c9f66be3a2b322b73b1d609d4dc6cfc8f410419e
cwparsons <cwparsons@users.noreply.github.com>
2024-04-09 21:53:14 +0000

    deploy: 7a2e5a3c88ad6cd3571a765faa3595e988c31f57

---
+++ assets/optimise-C0g5UgrS.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./index-DsIt1ZA5.js";const o={level:2,interlace:!1,optimiseAlpha:!1};async function s(t){const{default:e,optimise:i}=await a(()=>import("./squoosh_oxipng-fEqegEMX.js"),__vite__mapDeps([]));return await e(t),i}let n;async function r(t){return n||(n=s(t)),n}async function c(t,e={}){const i={...o,...e};return(await r())(new Uint8Array(t),i.level,i.interlace,i.optimiseAlpha).buffer}export{c as default,r as init};
